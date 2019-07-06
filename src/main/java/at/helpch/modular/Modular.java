554e77c525ef90ee8392b8e12aa8b64674e866a7
PiggyPiglet <noreply@piggypiglet.me>
2019-07-06 16:22:51 +0800

    Skeleton

---
+++ src/main/java/at/helpch/modular/Modular.java
@@ -0,0 +1,14 @@
+package at.helpch.modular;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+// ------------------------------
+// Copyright (c) PiggyPiglet 2019
+// https://www.piggypiglet.me
+// ------------------------------
+public final class Modular extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+}
